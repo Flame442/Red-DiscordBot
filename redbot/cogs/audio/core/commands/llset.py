a1c059b70ea6d33fbd6b17c0d8a9c08dbcf53219
Flame442 <34169552+Flame442@users.noreply.github.com>
2022-02-11 16:43:06 -0500

    Update llset.py

--- redbot/cogs/audio/core/commands/llset.py
+++ redbot/cogs/audio/core/commands/llset.py
@@ -15,7 +15,7 @@ _ = Translator("Audio", Path(__file__))
 
 
 class LavalinkSetupCommands(MixinMeta, metaclass=CompositeMetaClass):
-    @commands.group(name="llsetup", aliases=["llset"])
+    @commands.group(name="llsetup", aliases=["llset"], hidden=True)
     @commands.is_owner()
     @commands.bot_has_permissions(embed_links=True)
     async def command_llsetup(self, ctx: commands.Context):
